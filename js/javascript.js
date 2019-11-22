d41432aa15431dfe3e103dcd6ce85108d04773c2
ivantasic <taskefull@gmail.com>
2019-11-22 23:08:18 +0100

    made detaljnije page

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,11 @@
-document.onscroll = function() {
-    if( $(window).scrollTop() > $('header').height() ) {
+document.onscroll = function () {
+    if ($(window).scrollTop() > $('header').height()) {
         $('nav > div.navbar-inverse').removeClass('navbar-relative-top').addClass('navbar-fixed-top');
-    }
-    else {
+    } else {
         $('nav > div.navbar-inverse').removeClass('navbar-fixed-top').addClass('navbar-relative-top');
     }
-    };
+};
 
-    $('.carousel').carousel({
-        interval: 8000
-    });
-
-
+$('.carousel').carousel({
+    interval: 8000
+});
