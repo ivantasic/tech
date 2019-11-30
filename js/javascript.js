6a07ea745819db014c3f4024f78a82c725fda51e
ivantasic <taskefull@gmail.com>
2019-11-30 15:49:20 +0100

    one keyboard added, minor changes and made buy button

--- js/javascript.js
+++ js/javascript.js
@@ -9,3 +9,23 @@ document.onscroll = function () {
 $('.carousel').carousel({
     interval: 8000
 });
+
+function narucite() {
+    alert("Uspešno ste naručili ovaj proizvod!");
+  }
+
+
+function openForm() {
+    document.getElementById("loginPopup").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("loginPopup").style.display= "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    var modal = document.getElementById('loginPopup');
+    if (event.target == modal) {
+      closeForm();
+    }
+  }
